dac0568d15a33cf397822a5c552197b2a91fb539
Templar-101 <hcokorie@gmail.com>
2026-02-09 14:45:39 +0000

    Initial commit: Terraform CI/CD project structure

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "henry" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name        = "henry-terraform-ci"
-    Environment = "dev"
-  }
-}
--- terraform.tfvars
+++
@@ -1,2 +0,0 @@
-region = "eu-west-2"
-ami_id = "ami-0737d2d50c7fece1b"
